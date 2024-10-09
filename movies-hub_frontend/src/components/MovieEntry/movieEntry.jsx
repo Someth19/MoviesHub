de77365221f55b92ac0f103bf03a0b166c866950
Hein <heinhtut161@gmail.com>
2024-10-09 10:08:02 +0630

    font-style

---
+++ movies-hub_frontend/src/components/MovieEntry/movieEntry.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieEntry = () => {
+    return (
+        <div>
+            <h1>Movie Entry</h1>
+            
+        </div>
+    )
+}
+
+export default MovieEntry;
