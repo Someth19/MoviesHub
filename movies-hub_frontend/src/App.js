3ea124060de8a3d491b6f0948ffc8db52fe8adae
Hein <heinhtut161@gmail.com>
2024-10-09 10:41:28 +0630

    BroswerRouter

--- movies-hub_frontend/src/App.js
+++ movies-hub_frontend/src/App.js
@@ -1,10 +1,28 @@
-import CastEntry from "./components/Cast/CastEntry";
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import CastEntry from './components/Cast/CastEntry';
+import MovieEntry from './components/MovieEntry/movieEntry'; 
 
 function App() {
   return (
-    <div className="App">
-      <CastEntry/>
-    </div>
+    <>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/castEntry">Cast Entry</Link>
+            </li>
+            <li>
+              <Link to="/movieEntry">Movie Entry</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/castEntry" element={<CastEntry />} />
+          <Route path="/movieEntry" element={<MovieEntry />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- movies-hub_frontend/src/components/MovieEntry/movieEntry.jsx
+++ movies-hub_frontend/src/components/MovieEntry/movieEntry.jsx
@@ -1,10 +1,78 @@
 import React from 'react';
 
 const MovieEntry = () => {
+    const [movie, setMovie] = useState({
+        title: '',
+        director: '',
+        year: '',
+        genre: ''
+    });
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setMovie((prev) => ({
+            ...prev,
+            [name]: value,
+        }))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        console.log(movie);
+        setMovie({
+            title: '',
+            director: '',
+            year: '',
+            genre: ''
+        });
+    }
     return (
         <div>
-            <h1>Movie Entry</h1>
-            
+            <h2>Movie Entry Form</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Title :</label>
+                    <input
+                        type="text"
+                        name="title"
+                        value={movie.title}
+                        onChange={handleInputChange}  
+                        required                  
+                    />
+                </div>
+                <div>
+                    <label>Director :</label>
+                    <input
+                        type="text"
+                        name="director"
+                        value={movie.director}
+                        onChange={handleInputChange}    
+                        required                
+                    />
+                </div>
+                <div>
+                    <label>Year :</label>
+                    <input
+                        type="number"
+                        name="year"
+                        value={movie.year}
+                        onChange={handleInputChange} 
+                        required                   
+                    />
+                </div>
+                <div>
+                    <label>Genre :</label>
+                    <input
+                        type="text"
+                        name="genre"
+                        value={movie.genre}
+                        onChange={handleInputChange}   
+                        required                 
+                    />
+                </div>
+                <button type="submit">Add Movie</button>
+            </form>
         </div>
     )
 }
